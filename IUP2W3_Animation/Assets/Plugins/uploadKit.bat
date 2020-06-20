7382bd79596ab2273184f06910be0f5a9b0635b4
Shane <shane@mshanew.net>
2020-06-20 19:43:00 -0400

    Mirrors

---
+++ IUP2W3_Animation/Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@S:\Documents\UnityProjects\IUP2W3\IUP2W3\IUP2W3_Animation\1499518703153184841_mirror.zip" -F "kit[game_engine_version]=20192" https://account.altvr.com/api/kits/1499518703153184841.json
