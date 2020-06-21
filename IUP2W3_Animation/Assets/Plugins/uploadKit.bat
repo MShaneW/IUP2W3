0973de251bfc4510e4acc6b4701dbc5c30020474
Shane <shane@mshanew.net>
2020-06-20 22:30:23 -0400

    Animated Banner

--- IUP2W3_Animation/Assets/Plugins/uploadKit.bat
+++ IUP2W3_Animation/Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@S:\Documents\UnityProjects\IUP2W3\IUP2W3\IUP2W3_Animation\1499626786525282473_mirror.zip" -F "kit[game_engine_version]=20192" https://account.altvr.com/api/kits/1499626786525282473.json
+curl -v -b cookie -X PUT -F "kit[zip]=@S:\Documents\UnityProjects\IUP2W3\IUP2W3\IUP2W3_Animation\1499710103882826103_banner.zip" -F "kit[game_engine_version]=20192" https://account.altvr.com/api/kits/1499710103882826103.json
