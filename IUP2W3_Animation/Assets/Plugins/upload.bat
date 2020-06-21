8fbb0d5430aeadb1929493e59c66507dfd331281
Shane <shane@mshanew.net>
2020-06-21 17:18:28 -0400

    Added world hub

--- IUP2W3_Animation/Assets/Plugins/upload.bat
+++ IUP2W3_Animation/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@S:\Documents\UnityProjects\IUP2W3\IUP2W3\IUP2W3_Animation\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1499648134911886257.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@S:\Documents\UnityProjects\IUP2W3\IUP2W3\IUP2W3_Animation\template.zip" -F "space_template[game_engine_version]=20192" https://account.altvr.com/api/space_templates/1500161699854942652.json
